3728003f21a35f7763572a3d237871ffe076412e
Kushagra Pandey <kushagra.15554@gmail.com>
2024-11-04 19:29:41 +0530

    Learned how to add fallbacks using suspense

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0b351805f7f81a56"; }
+/******/ 		__webpack_require__.h = function() { return "9661525cdd10ee82"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
